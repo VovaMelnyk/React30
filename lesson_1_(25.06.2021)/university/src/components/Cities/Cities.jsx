b379ab70e94d603d86f1caeefc727f5dcda17343
Vova Melnyk <hagmen4339@gmail.com>
2021-07-02 19:28:59 +0300

    prepare template

--- lesson_1_(25.06.2021)/university/src/components/Cities/Cities.jsx
+++ lesson_1_(25.06.2021)/university/src/components/Cities/Cities.jsx
@@ -12,8 +12,52 @@ function Cities() {
       </Box>
       <CitiesList />
       <Button text={"Добавить город"} />
+      {/* <form>
+        <input type="text" onChange={onChange} value={input} />
+      </form> */}
     </Container>
   );
 }
 
 export default Cities;
+
+// import React, { Component } from "react";
+// import Button from "../Button/Button";
+// import { Container, Box, Img, Title } from "./Cities.styled";
+// import CitiesList from "./CitiesList/CitiesList";
+// import src from "./img/pin.png";
+// export default class Cities extends Component {
+//   state = {
+//     value: "",
+//   };
+
+//   inputHandler = (e) => {
+//     console.log(e.target.value);
+//     this.setState({
+//       value: e.target.value,
+//     });
+//   };
+
+//   render() {
+//     console.log("test", this.state.value);
+
+//     return (
+//       <Container>
+//         <Box>
+//           <Img src={src} alt="cat" />
+//           <Title>Города</Title>
+//         </Box>
+//         <CitiesList />
+//         <Button text={"Добавить город"} />
+//         <form>
+//           <input
+//             type="text"
+//             onChange={this.inputHandler}
+//             value={this.state.value}
+//             name="name"
+//           />
+//         </form>
+//       </Container>
+//     );
+//   }
+// }
